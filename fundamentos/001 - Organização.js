af0fe7da2d83f258bc7e8f5ede5b003bda3a5e0d
Gabriel Gato <70043907+Gato-Gabriel@users.noreply.github.com>
2020-12-25 15:00:58 -0300

    Create 001 - Organização.js

---
+++ fundamentos/001 - Organização.js
@@ -0,0 +1,16 @@
+/* Organização de um programa em JS */
+/* JavaScript é organizado em sentenças de código, dentro de blocos de código, delimitados por chaves. */
+console.log("Linha de código")
+
+{
+    /* Isso é um bloco de código. Serve para agrupar e organizar conjuntos de sentanças */
+    console.log("Olá mundo!")
+}
+// Isto é um comentário. Tudo que estiver dentro de um não será interpretado, mas completamente ignorado pela linguagem
+/* Isto é um comentário. Tudo que estiver dentro de um não será
+   interpretado, mas completamente ignorado pela linguagem */
+/* Dicas:
+ * 1: Evite comentários idiotas, que reiterem algo óbvio;
+ * 2: Prefira código claro e limpo a comentários;
+ * 3: Comente tudo que achar necessário, todavia.
+*/
